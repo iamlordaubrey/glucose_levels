7ddff2e94eb8b744e0e845ff30c326a1fe996b62
Chiemeka Alim <lord.chiemy@gmail.com>
2024-04-17 18:10:32 +0100

    Remove commented lines

--- glucose_levels/tests.py
+++ glucose_levels/tests.py
@@ -1,11 +1,9 @@
-from django.contrib.auth import get_user_model
 from django.test import TestCase
 from rest_framework import status
 from rest_framework.reverse import reverse
 from rest_framework.test import APIClient
 
 from glucose_levels.models import GlucoseLevel, CustomUser
-from glucose_levels.serializers import CustomUserSerializer
 
 
 class GlucoseLevelsRetrieveAndPaginationTest(TestCase):
